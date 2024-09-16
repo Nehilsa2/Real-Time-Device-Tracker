cb6d7b07abaf6cd34d032dd653538d4217da34dc
Nehilsa2 <nehilsahu281@gmail.com>
2024-09-16 14:59:38 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ io.on("connection", function(socket) {
 });
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
